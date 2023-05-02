88f6c3bf6150fcb4e2371f7a8aa5d83b206bb31a
Kevin Verhaegh <kevin.verhaegh@ukaea.uk>
2023-05-02 16:40:51 +0100

    Added setup.py for installation.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CRUMPET',
+    version='',
+    packages=['CRUMPET'],
+    url='',
+    license='',
+    author='holm',
+    author_email='',
+    description=''
+)
